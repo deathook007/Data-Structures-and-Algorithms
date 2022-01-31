909b7e8b854fafcdf6fec7c5cd8e910eca767bad
deathook <deepak.shivay.bhatt@gmail.com>
2022-01-31 23:48:57 +0530

    Majority Element > N/3

---
+++ Array/Majority Element2.cpp
@@ -0,0 +1,85 @@
+/* 
+Majority Element > N/2
+Approach - Using unordered_map
+Time Complexity : O(N) or worst case O(NlogN)
+Space Complexity : O(N)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void majorityElement(int arr[], int size){
+    unordered_map <int, int> mpp;
+    vector <int> v;
+    for(int i=0; i<size; ++i){
+        int count = ++mpp[arr[i]];
+        if(count > size/3){
+            v.push_back(arr[i]);
+        }
+    }
+    for(int i=0; i<v.size(); ++i){
+        cout<<v[i]<<" ";
+    }
+}
+
+int main(){
+    int arr[] = {10, 10, 20, 30, 10, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    majorityElement(arr, n);
+    return 0;
+}
+/* 
+Majority Element > N/2
+Approach - Boyer Mores Voting Algorithm
+Time Complexity : O(N)
+Space Complexity : O(1)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void majorityElement(int arr[], int size){
+    int count1 = 0;
+    int count2 = 0;
+    int candidate1 = 0;
+    int candidate2 = 0;
+    for(int i=0; i<size; i++){
+        if(count1 == 0){
+            candidate1 = arr[i];
+        }else if(count2 == 0){
+            candidate2 = arr[i];
+        }else if(candidate1 == arr[i]){
+            count1++;
+        }else if(candidate2 == arr[i]){
+            count2++;
+        }else{
+            count1--;
+            count2--;
+        }
+    }
+    vector <int> v;
+    count1 = 0;
+    count2 = 0;
+    for(int i=0; i<size; i++){
+        if(arr[i] == candidate1){
+            count1++;
+        }else if(arr[i] == candidate2){
+            count2++;
+        }
+    }
+    if(count1 > size/3){
+        v.push_back(candidate1);
+    }
+    if(count2 > size/3){
+        v.push_back(candidate2);
+    }
+    if(v.size() == 0){
+        cout<<"-1";
+    }
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i]<<" ";
+    }
+}
+
+int main(){
+    int arr[] = {10, 10, 20, 30, 10, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    majorityElement(arr, n);
+    return 0;
+}

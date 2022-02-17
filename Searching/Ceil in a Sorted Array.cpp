d751668b9241801f5ea822f615f9ff7051a3d913
deathook <deepak.shivay.bhatt@gmail.com>
2022-02-17 16:48:19 +0530

    Ceil in a Sorted Array

---
+++ Searching/Ceil in a Sorted Array.cpp
@@ -0,0 +1,33 @@
+/*
+Floor in a Sorted Array
+Approach - Binary search
+Time Complexity : O(logN)
+Space Complexity : O(1)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int findCeil(int arr[], int size, int target){
+    int start = 0;
+    int end = size - 1;
+    while(start <= end){
+        int middle = start + (end - start) / 2;
+        if(arr[middle] < target){
+            start = middle + 1;
+        }else if(arr[middle] > target){
+            end = middle - 1;
+        }else{
+            return middle;
+        }
+    }
+    return start;
+}
+
+
+int main(){
+    int arr[] = {1, 2, 8, 10, 11, 12, 19};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int X = 0;
+    cout<< findCeil(arr, n, X);
+    return 0;
+}
